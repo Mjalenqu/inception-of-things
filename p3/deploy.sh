17f46bf8a9b1e527423872e343d5e2215ca35715
mjalenqu <mjalenques@proton.me>
2022-09-14 16:58:09 +0000

    P3 in progress

--- p3/deploy.sh
+++ p3/deploy.sh
@@ -14,3 +14,4 @@ sleep 5
 
 echo "Install ArgoCD in namespace argocd"
 kubectl apply -n argocd -f /home/vagrant/install-argo.yaml
+
--- p3/p3.sh
+++ p3/p3.sh
@@ -48,6 +48,6 @@ sudo usermod -aG docker vagrant
 echo "Install K3d"
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-
+sleep 5
 
 
