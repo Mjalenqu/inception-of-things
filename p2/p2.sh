6ac622db52bc870300d66bc1490c9b030673d010
mjalenqu <mjalenques@proton.me>
2022-09-28 09:42:29 +0000

    typo in app.yaml

--- p2/p2.sh
+++ p2/p2.sh
@@ -1,8 +1,8 @@
 sudo systemctl disable --now firewalld
 echo "Running yum update"
 sudo yum update -y
-echo "Install net-tools"
-sudo yum install -y net-tools
+# echo "Install net-tools"
+# sudo yum install -y net-tools
 echo "Copy ssh key"
 mkdir -p /home/vagrant/.ssh
 cat /tmp/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
@@ -14,14 +14,14 @@ sudo curl -sfL https://get.k3s.io | sh -s - server \
 	--tls-san 192.168.42.110
 
 
-echo "Installing kubectl"
-cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
-[kubernetes]
-name=Kubernetes
-baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
-enabled=1
-gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-sudo yum install -y kubectl
+# echo "Installing kubectl"
+# cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+# [kubernetes]
+# name=Kubernetes
+# baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+# enabled=1
+# gpgcheck=1
+# gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+# EOF
+# sudo yum install -y kubectl
 kubectl apply -f .
